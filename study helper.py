29bbb95887173180e3e6fa841f17b0823f890981
JaCrispy <64677705+JaCrispy4939@users.noreply.github.com>
2021-05-30 21:00:44 -0400

    Add files via upload

--- study helper.py
+++ study helper.py
@@ -2,11 +2,13 @@ from googlesearch import search
 import time
 import random
 import webbrowser
+import urllib.request
+from bs4 import BeautifulSoup
 
 while True:
     print("STUDY HELPER")
     print("")
-    tool = input("1. Google Search, 2. Calculator, 3. Exit: ")
+    tool = input("1. Google Search, 2. Calculator, 3. definition finder, 4. Exit: ")
 
     if tool == "1":
         query = input("what do you want to search: ")
@@ -45,8 +47,41 @@ while True:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
             print(num1, "/", num2, "=", divide(num1, num2))
-        
+
     if tool == "3":
+        word = input('Enter the word of which you want to find the meaning: ')
+
+        url = "https://www.vocabulary.com/dictionary/" + word + ""
+        htmlfile = urllib.request.urlopen(url)
+        soup = BeautifulSoup(htmlfile, 'lxml')
+        soup1 = soup.find(class_="short")
+
+        # If certain word's meaning is not found
+        try:
+            soup1 = soup1.get_text()
+        except AttributeError:
+            print('Cannot find such word! Check spelling.')
+            
+
+        # Print short meaning
+        print ('-' * 25 + '->',word,"<-" + "-" * 25)
+        print ("SHORT MEANING: ",soup1)
+        print ('-' * 65)
+
+        # Print long meaning
+        soup2 = soup.find(class_="long")
+        soup2 = soup2.get_text()
+
+        print ('-' * 65)
+
+        # Print instances like Synonyms, Antonyms, etc.
+        soup3 = soup.find(class_="instances")
+        txt = soup3.get_text()
+        txt1 = txt.rstrip()
+
+        print (' '.join(txt1.split()))
+
+    if tool == "4":
         sure = input
         print("goodbye")
         time.sleep(5)
